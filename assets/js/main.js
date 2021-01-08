9176a4b1af244e5ad2713b72a4038c3b087931bc
Kritika Iyer <kiyer@wpi.edu>
2021-01-08 18:04:01 -0500

    Added drop down for experience

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,8 +20,26 @@ jQuery(document).ready(function($) {
 
 });
 
-var acc = document.getElementsByClassName("accordion");
+var acc = document.getElementsByClassName("section-accordion");
 var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    }
+}
+
+acc = document.getElementsByClassName("accordion");
 
 for (i = 0; i < acc.length; i++) {
     acc[i].onclick = function(){

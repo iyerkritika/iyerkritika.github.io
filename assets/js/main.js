a39d94ba5582e60c90a1352444720ab5742a6349
Kritika Iyer <kiyer@wpi.edu>
2021-01-10 00:26:58 -0500

    Added plus minus signs for accordian

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,3 +56,19 @@ for (i = 0; i < acc.length; i++) {
         }
     }
 }
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
